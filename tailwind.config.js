fd2c728dd4980f7dda8b18c7d29f0ed481482896
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2023-02-15 03:32:12 +0700

    add responsive tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '540px',
+      md: '720px',
+      lg: '960px',
+      xl: '1140px',
+      '2xl': '1320px'
+    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
