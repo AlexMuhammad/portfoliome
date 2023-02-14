38b14a5a33e1d088f39737b2a303745a08d01f14
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2023-02-15 03:33:01 +0700

    [FEATURE]: eslit setup for clean code (#1)
    * feat: eslit setup for clean code
    
    * feat: install tailwindcss
    
    * add responsive tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React JS</h1>
+      <h1 className="text-red-400 text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
