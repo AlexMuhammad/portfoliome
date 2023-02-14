f17b032e4ad7f6c5f311b0fee70e5706ba4c30b3
AlexMuhammad <alexmuhammad@students.amikom.ac.id>
2023-02-15 03:22:48 +0700

    feat: install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React JS</h1>
+      <h1 className="text-red-400 text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
