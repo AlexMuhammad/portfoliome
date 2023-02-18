88c9e8c6c929d0bc67f9748a5ad4a51039b9c03c
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2023-02-19 03:45:33 +0700

    [FIX]: Navbar responsive issue (#3)
    * feat: eslit setup for clean code
    
    * feat: install tailwindcss
    
    * add responsive tailwindcss
    
    * template home starter
    
    * fix: navbar responsive
    
    * fix conflict merge #2

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -4,12 +4,12 @@ import CardFirst from '../assets/card-1.png';
 
 const CardProject = () => {
   return (
-    <>
-      <div className="w-[49%] bg-white border-2 border-primaryLight rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:  relative">
+    <div className="w-full grid md:grid-cols-2 gap-4">
+      <div className="w-full bg-white rounded-lg duration-200 shadow group/item relative">
         <a href="#">
           <img className="w-full rounded-t-lg" src={CardFirst} alt="" />
         </a>
-        <div className="w-full h-1/5 flex items-center justify-center bg-primaryLight p-5 rounded-b-lg absolute bottom-0 bg-opacity-70">
+        <div className="w-full h-1/5 flex items-center justify-center rounded-b-lg bg-primaryLight bottom-0 bg-opacity-70 opacity-0 group-hover/item:opacity-100 group-hover/item:duration-500 absolute">
           <a href="#">
             <h5 className="mb-2 text-md font-bold tracking-tight text-[white] dark:text-white">
               Lorem-ipsum
@@ -17,11 +17,11 @@ const CardProject = () => {
           </a>
         </div>
       </div>
-      <div className="w-[49%] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 relative">
+      <div className="w-full bg-white rounded-lg duration-200 shadow group/item relative">
         <a href="#">
           <img className="w-full rounded-t-lg" src={CardFirst} alt="" />
         </a>
-        <div className="w-full h-1/5 flex items-center justify-center bg-primaryLight p-5 rounded-b-lg absolute bottom-0 bg-opacity-70">
+        <div className="w-full h-1/5 flex items-center justify-center rounded-b-lg bg-primaryLight bottom-0 bg-opacity-70 opacity-0 group-hover/item:opacity-100 group-hover/item:duration-500 absolute">
           <a href="#">
             <h5 className="mb-2 text-md font-bold tracking-tight text-[white] dark:text-white">
               Lorem-ipsum
@@ -29,7 +29,7 @@ const CardProject = () => {
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
